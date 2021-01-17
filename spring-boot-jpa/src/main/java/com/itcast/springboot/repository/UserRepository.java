958ffd269571e140c14c4d20fe4c284548895652
hesensen <hesensen@hztianque.com>
2021-01-17 21:47:18 +0800

    分页、模糊、时间、范围查找

--- spring-boot-jpa/src/main/java/com/itcast/springboot/repository/UserRepository.java
+++ spring-boot-jpa/src/main/java/com/itcast/springboot/repository/UserRepository.java
@@ -1,16 +1,22 @@
 package com.itcast.springboot.repository;
 
 import com.itcast.springboot.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-//继承JpaRepository来完成对数据库的操作
+/**
+ * 继承JpaRepository来完成对数据库的操作
+ */
 public interface UserRepository extends JpaRepository<User,Integer> {
 
 
+    Page<User> findAll(Specification<User> userSpecification, Pageable pageable);
 
     List<User> findUsersByLastNameIsStartingWithAndEmailContains(String lastName,String email);
 
@@ -18,9 +24,16 @@ public interface UserRepository extends JpaRepository<User,Integer> {
 
     List<User> findUsersByLastNameIsEndingWith(String lastName);
 
+    /**
+     * 表名不要写数据库中的值,使用entity的类名
+     * 属性名也同样不要使用数据库值，使用entity的属性名
+     * @param userId
+     * @param email
+     */
     @Modifying
-    @Query("update User u set u.email=?2 where  u.id=?1")
-    void updateEmailByUserId(Integer userId, String email);
+    @Query("update User u set u.email=?2, u.lastName=?3 where  u.id=?1")
+    void updateEmailByUserId(Integer userId, String email, String lastName);
 
-
+    @Query("select id,lastName,email,sex,birthday from User where id=?1")
+    List<User> findUserById(Integer id);
 }
--- spring-boot-jpa/src/test/java/com/itcast/springboot/repository/UserRepositoryTest.java
+++ spring-boot-jpa/src/test/java/com/itcast/springboot/repository/UserRepositoryTest.java
@@ -8,9 +8,19 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.*;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 @RunWith(SpringRunner.class)
@@ -114,7 +124,7 @@ public class UserRepositoryTest {
     }
 
     /**
-     * 条件查询
+     * 条件查询,自定义查询
      */
     @Test
     public void findUsersByLastNameIsStartingWithAndEmailContains(){
@@ -122,8 +132,9 @@ public class UserRepositoryTest {
         String email = "3110708879@qq.com";
         //List<User> users = userRepository.findUsersByLastNameIsStartingWithAndEmailContains(name, email);
         //List<User> users = userRepository.findUsersByLastNameIsStartingWith(name);
-        List<User> users = userRepository.findUsersByLastNameIsEndingWith(name);
-        System.out.println(users);
+        //List<User> users = userRepository.findUsersByLastNameIsEndingWith(name);
+        List<User> user = userRepository.findUserById(4);
+        System.out.println(user);
     }
 
     /**
@@ -133,7 +144,7 @@ public class UserRepositoryTest {
     @Transactional
     public void updateEmailByUserId(){
 
-        userRepository.updateEmailByUserId(6,"133789456@qq.com");
+        userRepository.updateEmailByUserId(6,"133789456@qq.com", "hssE");
         System.out.println("------修改成功-----");
     }
 
@@ -179,17 +190,91 @@ public class UserRepositoryTest {
 
     @Test
     public void pageQuery(){
-        int page = 0,size=10;
+        // page 0是第一页 1是第二页 ...
+        int page = 0,size = 10;
+        //排序
         Sort sort = new Sort(Sort.Direction.DESC, "id");
-
+        //分页条件
         PageRequest pageRequest = PageRequest.of(page, size, sort);
 
         User user = new User();
         user.setEmail("3110708871@qq.com");
-        Example<User> example = Example.of(user);
+        user.setLastName("hss");
+
+        ExampleMatcher exampleMatcher = ExampleMatcher.matchingAny();
+        //原写法
+        exampleMatcher = exampleMatcher.withMatcher("lastName", ExampleMatcher.GenericPropertyMatchers.startsWith())
+                .withMatcher("email",ExampleMatcher.GenericPropertyMatchers.exact());
+
+        Example<User> example = Example.of(user,exampleMatcher);
 
         //Page<User> userPage = userRepository.findAll(pageRequest);
         Page<User> userPage = userRepository.findAll(example, pageRequest);
+        System.out.println("总记录数：" + userPage.getTotalElements());
+        System.out.println("总页数：" + userPage.getTotalPages());
+        System.out.println(userPage.getContent());
+    }
+
+    @Test
+    public void pageQuery1(){
+        String lastName = "hss";
+
+        Integer sex = 1;
+
+        String startTime = "2021-1-11";
+        String endTime = "2021-1-11";
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat sdfmat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        // page 0是第一页 1是第二页 ...
+        int page = 0,size = 10;
+        //排序
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        //分页条件
+        PageRequest pageRequest = PageRequest.of(page, size, sort);
+
+        Specification<User> specification = new Specification<User>() {
+            @Override
+            public Predicate toPredicate(Root<User> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder cb) {
+                List<Predicate> predicate = new ArrayList<>();
+                try {
+                    //模糊匹配
+                    if(StringUtils.isNoneBlank(lastName)){
+                        predicate.add(cb.like(root.get("lastName").as(String.class), "%" + lastName + "%"));
+                    }
+                    if(sex != null){
+                        //精准查找
+                        //predicate.add(cb.equal(root.get("sex").as(String.class), sex));
+                        //小于
+                        predicate.add(cb.lt(root.get("sex").as(Integer.class),0));
+                        //大于
+                        predicate.add(cb.gt(root.get("sex").as(Integer.class),3));
+                    }
+                    //日期范围
+                    if(StringUtils.isNotEmpty(startTime) && StringUtils.isNotEmpty(endTime)){
+                        /**
+                         * 使用between and 可以提高语句的简洁度
+                         * between and包含临界值（即包含两个边界值）
+                         * between and的两个临界值不要乱调换顺序（数值小的放前面，数值大的放后面）
+                         */
+                        predicate.add(cb.between(root.get("birthday"),
+                                sdf.parse(startTime),
+                                sdf.parse(endTime)));
+                    }
+
+                    Predicate[] pre = new Predicate[predicate.size()];
+                    return criteriaQuery.where(predicate.toArray(pre)).getRestriction();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        };
+
+        Page<User> userPage = userRepository.findAll(specification, pageRequest);
+        System.out.println("总记录数：" + userPage.getTotalElements());
+        System.out.println("总页数：" + userPage.getTotalPages());
         System.out.println(userPage.getContent());
     }
 }
