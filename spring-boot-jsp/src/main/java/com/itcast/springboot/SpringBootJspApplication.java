e2ca852443cdd64f49774267a426a6a22c6b6024
‘HeTreeTerr’ <3110708879@qq.com>
2021-10-21 11:39:28 +0800

    springBoot jvm参数调优

--- spring-boot-jsp/src/main/java/com/itcast/springboot/SpringBootJspApplication.java
+++ spring-boot-jsp/src/main/java/com/itcast/springboot/SpringBootJspApplication.java
@@ -3,6 +3,10 @@ package com.itcast.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * springBoot jar/war jvm参数调优
+ * java -server -Xms512m -Xmx512m -jar spring-boot-jsp-0.0.1-SNAPSHOT.war
+ */
 @SpringBootApplication
 public class SpringBootJspApplication {
 
