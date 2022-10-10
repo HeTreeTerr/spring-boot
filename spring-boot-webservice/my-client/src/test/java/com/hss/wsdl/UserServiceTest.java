4797d49c1e4f9309fcfb81ae2158145a427e114e
hss1552 <hesensen@tzt.cn>
2022-10-10 11:00:11 +0800

    spring boot WebService，基础使用

--- spring-boot-webservice/my-client/src/test/java/com/hss/wsdl/UserServiceTest.java
+++ spring-boot-webservice/my-client/src/test/java/com/hss/wsdl/UserServiceTest.java
@@ -14,15 +14,25 @@ import javax.annotation.Resource;
 @Slf4j
 public class UserServiceTest {
 
+    /**
+     * 在一个方法中连续调用多次WebService接口，每次调用前需要重置上下文。
+     */
+    private static final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
     @Test
     public void getUserName() {
+        //在获取连接之前 还原上下文
+        Thread.currentThread().setContextClassLoader(classLoader);
+
         String url = "http://localhost:8001/ws/user?wsdl";
         String methodName = "getUserName";
         String params = "hss";
-
         try {
             String res = ClientUtil.callWebSV(url, methodName, params);
             log.info("res:{}",res);
+
+            String res1 = ClientUtil.callWebSV(url, methodName, params);
+            log.info("res1:{}",res1);
         } catch (Exception e) {
             log.error("调用失败：",e);
         }
